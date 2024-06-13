4bd636e35e2751e73b048f986dbe44ee17c359d4
samosaaa <samra_adl@outlook.fr>
2024-06-13 20:08:29 +0200

    redirect gestion directement

--- budgetTracking/budgetTracking/gestionController.py
+++ budgetTracking/budgetTracking/gestionController.py
@@ -24,8 +24,23 @@ def saveActivity(request):
     if idParent != "0":
         updateBudgetDepense(idParent)
         updateReste(idParent)
+    
+      # Créez le message que vous voulez afficher dans la console
+    message = (f'Données reçues : \n'
+               f'idParent : {idParent}\n'
+               f'Libellé : {libelle}\n'
+               f'Nom : {nom}\n'
+               f'Budget Initial : {budget_initial}\n'
+               f'Budget Dépensé : {budget_depense}\n'
+               f'Reste : {reste}\n'
+               f'Projection Partielle : {projection_partielle}\n'
+               f'Reste après Projection : {reste_apres_projection}\n'
+               f'Commentaires : {commentaires}')
+    
+    # Imprimez le message dans la console
+    print(message)
 
-    return HttpResponse(f'Données reçues : <br>idParent : {idParent}<br>Libellé : {libelle}<br>Nom : {nom}<br>Budget Initial : {budget_initial}<br>Budget Dépensé : {budget_depense}<br>Reste : {reste}<br>Projection Partielle : {projection_partielle}<br>Reste après Projection : {reste_apres_projection}<br>Commentaires : {commentaires}')
+    return redirect('/gestion')
 
 def updateBudgetDepense(id):
     activity = gestionModel.getActivityById(id)
